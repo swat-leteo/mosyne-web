30b5e2735c99e685891ee718cc6ecc0851d91177
Jose Garzon <jd.garzon12@gmail.com>
2020-12-03 08:30:08 -0500

    <FEATURE> landing responsive

--- components/ui/Bullets.jsx
+++ components/ui/Bullets.jsx
@@ -1,47 +1,60 @@
-import React from "react";
-import styled from "@emotion/styled";
+//----- import Libraries
+import React from 'react';
+import styled from '@emotion/styled';
+
+//----- import components
+import { Media } from '../../types/mediaquery';
 
 const BulletsContainer = styled.div`
-  position: relative;
-  display: flex;
-  justify-content: space-around;
-  width: 100%;
-  margin-top: 40px;
-  hr {
-    position: absolute;
-    z-index: -1;
-    top: 14px;
-    left: 20px;
-    right: 20px;
-    border: 1px solid var(--blue);
-  }
+	position: relative;
+	display: flex;
+	justify-content: space-around;
+	width: 100%;
+	margin-top: 40px;
+	hr {
+		position: absolute;
+		z-index: -1;
+		top: 14px;
+		left: 20px;
+		right: 20px;
+		border: 1px solid var(--blue);
+		@media ${Media.desktop} {
+			top: 30px;
+			border: 2px solid var(--blue);
+		}
+	}
 
-  h3 {
-    width: 25px;
-    height: 25px;
-    text-align: center;
-    font-size: 10px;
-    background-color: var(--blue);
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    border-radius: 60%;
-    color: var(--white);
-    &:nth-of-type(${(props) => props.bullet}) {
-      background-color: var(--purple1);
-    }
-  }
+	h3 {
+		width: 25px;
+		height: 25px;
+		text-align: center;
+		font-size: 10px;
+		background-color: var(--blue);
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		border-radius: 60%;
+		color: var(--white);
+		@media ${Media.desktop} {
+			width: 50px;
+			height: 50px;
+			font-size: 16px;
+		}
+		&:nth-of-type(${(props) => props.bullet}) {
+			background-color: var(--purple1);
+		}
+	}
 `;
 
 const Bullets = ({ bullet }) => {
-  return (
-    <BulletsContainer bullet={bullet}>
-      <hr />
-      <h3>1</h3>
-      <h3>2</h3>
-      <h3>3</h3>
-    </BulletsContainer>
-  );
+	return (
+		<BulletsContainer bullet={bullet}>
+			<hr />
+			<h3>1</h3>
+			<h3>2</h3>
+			<h3>3</h3>
+		</BulletsContainer>
+	);
 };
 
 export default Bullets;
--- components/ui/ButtonGoogle.jsx
+++ components/ui/ButtonGoogle.jsx
@@ -1,69 +1,70 @@
-import React, { useContext } from "react";
-import styled from "@emotion/styled";
-import GoogleLogin from "react-google-login";
+import React, { useContext } from 'react';
+import styled from '@emotion/styled';
+import GoogleLogin from 'react-google-login';
 
-import IconGoogle from "./icons/IconGoogle";
-import AuthContext from "../../context/auth/authContext";
+import IconGoogle from './icons/IconGoogle';
+import AuthContext from '../../context/auth/authContext';
 
 const Button = styled(GoogleLogin)`
-  width: ${(props) => props.width};
-  margin-bottom: 10px;
-  display: flex !important;
-  justify-content: space-between;
-  box-shadow: ${(props) =>
-    props.shadow && "inset 0px -2px 1px rgba(0, 0, 0, 0.25)"};
-  border-radius: 8px !important;
-  background-color: ${(props) => props.bgColor}!important;
-  border-color: ${(props) => props.borderColor}!important;
-  color: ${(props) => props.textColor}!important;
-  font-size: 12px;
-  line-height: 14px;
-  > div {
-    display: none;
-  }
-  > span {
-    width: 100%;
-    display: flex;
-    justify-content: space-between;
-    svg {
-      margin: 0 5px;
-    }
-  }
-  &:last-of-type {
-    margin-bottom: 0;
-  }
-  &:hover {
-    cursor: pointer;
-  }
+	width: ${(props) => props.width};
+	margin-bottom: 10px;
+	display: flex !important;
+	justify-content: space-between;
+	box-shadow: ${(props) =>
+		props.shadow && 'inset 0px -2px 1px rgba(0, 0, 0, 0.25)'};
+	border-radius: 8px !important;
+	background-color: ${(props) => props.bgColor}!important;
+	border-color: ${(props) => props.borderColor}!important;
+	color: ${(props) => props.textColor}!important;
+	font-size: 12px;
+	line-height: 14px;
+	> div {
+		display: none;
+	}
+	> span {
+		padding: 15px 25px !important;
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		svg {
+			margin: 0 5px;
+		}
+	}
+	&:last-of-type {
+		margin-bottom: 0;
+	}
+	&:hover {
+		cursor: pointer;
+	}
 `;
 
 const ButtonGoogle = ({ bgColor, textColor, borderColor, shadow, width }) => {
-  const { registrarGoogle } = useContext(AuthContext);
+	const { registrarGoogle } = useContext(AuthContext);
 
-  const responseGoogle = ({ profileObj: { email, name, familyName } }) => {
-    registrarGoogle({
-      email,
-      firstname: name,
-      lastname: familyName ? familyName : "",
-    });
-  };
+	const responseGoogle = ({ profileObj: { email, name, familyName } }) => {
+		registrarGoogle({
+			email,
+			firstname: name,
+			lastname: familyName ? familyName : '',
+		});
+	};
 
-  return (
-    <Button
-      bgColor={bgColor}
-      textColor={textColor}
-      borderColor={borderColor}
-      width={width}
-      shadow={shadow}
-      clientId="730257169575-9dcdlk66l2u807j0dobgbhjsf4phhti1.apps.googleusercontent.com"
-      onSuccess={responseGoogle}
-      onFailure={responseGoogle}
-      cookiePolicy={"single_host_origin"}
-    >
-      <IconGoogle />
-      Registrate con Google
-    </Button>
-  );
+	return (
+		<Button
+			bgColor={bgColor}
+			textColor={textColor}
+			borderColor={borderColor}
+			width={width}
+			shadow={shadow}
+			clientId="730257169575-9dcdlk66l2u807j0dobgbhjsf4phhti1.apps.googleusercontent.com"
+			onSuccess={responseGoogle}
+			onFailure={responseGoogle}
+			cookiePolicy={'single_host_origin'}
+		>
+			<IconGoogle />
+			Registrate con Google
+		</Button>
+	);
 };
 
 export default ButtonGoogle;
